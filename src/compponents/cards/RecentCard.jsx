23e007a67a3d1f10341608e6a66c00042e6855a4
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-11-19 19:11:17 -0400

    shadow

--- src/compponents/cards/RecentCard.jsx
+++ src/compponents/cards/RecentCard.jsx
@@ -6,7 +6,7 @@ function RecentCard({ data }) {
 
   return (
     <div className="grid place-items-center w-[300px] h-[400px] border rounded-2xl py-[2rem] bg-gradient-to-b from-sky-400 to-cyan-200 text-white font-bold shadow-md">
-      <img src={recent.icon} alt="clima" className="h-24" />
+      <img src={recent.icon} alt="clima" className="h-24 drop-shadow-md" />
       <h1>{recent.temp}<sup>C</sup></h1>
       <h3>({recent.text})</h3>
       <h1>{recent.name}</h1>
